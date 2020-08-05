93a5aacf554d76b5eede803e00bb0d7edcee9262
Arnaud BODEL <contact@osw3.net>
2020-08-05 11:58:24 +0200

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdsComponent } from './components/ads/ads.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
