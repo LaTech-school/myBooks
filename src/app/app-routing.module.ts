91a9c5cdb6c39aeea37998fd5f00c3f8203fdc38
Arnaud BODEL <contact@osw3.net>
2020-08-05 13:02:43 +0200

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { CComponent } from './pages/c/c.component';
+
+const routes: Routes = [
+  // Route de base - Page A
+  {
+    path: '',
+    component: AComponent
+  },
+
+  // Page - B
+  {
+    path: 'page-b',
+    component: BComponent
+  },
+
+  // Page - C
+  {
+    path: 'page-c',
+    component: CComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; // Import du fichier de routage
 import { AppComponent } from './app.component';
 import { AdsComponent } from './components/ads/ads.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { CComponent } from './pages/c/c.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdsComponent,
-    SidebarComponent
+    SidebarComponent,
+    AComponent,
+    BComponent,
+    CComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // import de la classe de routage
   ],
   providers: [],
   bootstrap: [AppComponent]
